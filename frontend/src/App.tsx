0ca6d5eeb53005e7ad94bb26b44356d79ab2e702
Khushijain018 <khushi.jain0018@gmail.com>
2024-07-06 20:57:11 +0530

    added feature to add hotel and e2e test for that feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,10 @@ import {
 import Layout from './layouts/Layout'
 import Register from './pages/Register';
 import SignIn from './pages/SignIn';
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 function App() {
+  const {isLoggedIn}=useAppContext();
   return (
      <Router>
       <Routes>
@@ -25,6 +28,13 @@ function App() {
         <Route path="/sign-in" element={<Layout>
           <SignIn/>
         </Layout>} />
+        {isLoggedIn && <>
+        <Route path="/add-hotel" element={
+          <Layout>
+            <AddHotel/>
+          </Layout>
+        }/>
+        </>}
       </Routes>
      </Router>
   )
