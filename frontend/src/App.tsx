9e356deb138c9a1c20b4c42ae5ce3649876f9f01
Khushijain018 <khushi.jain0018@gmail.com>
2024-07-17 17:21:53 +0530

    home page having latest destination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Navigate,
 } from "react-router-dom";
 import Layout from './layouts/Layout'
 import Register from './pages/Register';
@@ -15,6 +16,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 function App() {
   const {isLoggedIn}=useAppContext();
@@ -22,7 +24,7 @@ function App() {
      <Router>
       <Routes>
         <Route path="/" element={<Layout>
-          <p> Home Page</p>
+          <Home/>
         </Layout>}/>
       <Route path="/search" element={<Layout>
           <Search/>
@@ -83,6 +85,7 @@ function App() {
             
           </>
         )}
+         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
      </Router>
   );
